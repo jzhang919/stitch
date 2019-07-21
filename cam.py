c87543e945dda8ee085906a4a572aea2ae2403f1
James Zhang <jzhan299@ford.com>
2019-07-20 18:02:31 -0700

    Testing out stitching.

--- cam.py
+++ cam.py
@@ -2,6 +2,7 @@ import cv2
 import argparse
 from stitcher import CVStitcher, Stitcher
 from dewarp import fishEyeUnwarp
+import time
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--debug", action='store_true',
@@ -13,61 +14,79 @@ ap.add_argument("--height", nargs='?', default=1080, type=int)
 args = ap.parse_args()
 
 
-def decode_fourcc(cc):
-    return "".join([chr((int(cc) >> 8 * i) & 0xFF) for i in range(4)])
+class cameraSystem:
 
+    def __init__(self, debug, stitch, res_width, res_height):
+        print("Initializing cameras...")
+        self.debug = debug
+        self.stitch = stitch
 
-defq capPropId(prop):
-    return getattr(cv2, "CAP_PROP_" + prop)
+        # TODO: Check why one ELP camera not able to use DSHOW?
+        self.cabinCam = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
+        self.extCam0 = cv2.VideoCapture(1 + cv2.CAP_DSHOW)
+        self.extCam1 = cv2.VideoCapture(2 + cv2.CAP_DSHOW)
+        self.extCam2 = cv2.VideoCapture(3 + cv2.CAP_DSHOW)
+        self.extCam3 = cv2.VideoCapture(4)
+        self.extCams = [self.extCam0, self.extCam1, self.extCam2, self.extCam3]
+
+        self.extFrames = []
+        self.pano = None
+        self.cabinFrame = None
+        self.f = fishEyeUnwarp(args.width, args.height,
+                               args.width, args.height, 180, 125)
+
+        for cam in self.extCams:
+            cam.set(self.capPropId("FOURCC"), cv2.VideoWriter_fourcc(*'MJPG'))
+            cam.set(self.capPropId("FRAME_WIDTH"), args.width)
+            cam.set(self.capPropId("FRAME_HEIGHT"), args.height)
+            cam.set(self.capPropId("FPS"), 20)
+        print("Initialization complete.")
+
+    def decode_fourcc(self, cc):
+        return "".join([chr((int(cc) >> 8 * i) & 0xFF) for i in range(4)])
+
+    def capPropId(self, prop):
+        return getattr(cv2, "CAP_PROP_" + prop)
+
+    # TODO: Process internal cabin camera.
+    def process(self):
+        while(True):
+            start_time = time.time()
+            # Capture frame-by-frame
+            self.extFrames.clear()
+            for cam in self.extCams:
+                ret, frame = cam.read()
+                frame = self.f.unwarp(frame)
+                self.extFrames.append(frame)
+            # Our operations on the frame come here
+            # Display the resulting frame
+            if self.stitch:
+                # s = CVStitcher(args.debug)
+                s = Stitcher(self.debug)
+                pano = s.stitch(self.extFrames)
+                # cv2.resize(pano, (320, 180), interpolation=cv2.INTER_AREA)
+                cv2.imshow("pano", pano)
+            if self.debug:
+                for i in range(len(self.extCams)):
+                    cv2.imshow('frame' + str(i),
+                               cv2.resize(self.extFrames[i], (480, 270),
+                                          interpolation=cv2.INTER_AREA))
+                for cam in self.extCams:
+                    print(self.decode_fourcc(cam.get(cv2.CAP_PROP_FOURCC)))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            fps = 1.0 / (time.time() - start_time)
+            print("[INFO] approx. FPS: {:.2f}".format(fps))
+
+        # Cleanly exit.
+        for cam in self.extCams:
+            cam.release()
+        cv2.destroyAllWindows()
 
 
 def main():
-    # Intel Realsense Camera needs CAP_DSHOW
-    print("Initializing cameras...")
-    cam0 = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
-    cam1 = cv2.VideoCapture(1 + cv2.CAP_DSHOW)
-    cam2 = cv2.VideoCapture(2 + cv2.CAP_DSHOW)
-    cam3 = cv2.VideoCapture(3 + cv2.CAP_DSHOW)
-    cam4 = cv2.VideoCapture(4)
-    cams = [cam1, cam2, cam3, cam4]
-    # f = fishEyeUnwarp(args.width, args.height, args.width, args.height, 180, 125)
-    frames = []
-    for cam in cams:
-        cam.set(capPropId("FOURCC"), cv2.VideoWriter_fourcc(*'MJPG'))
-        cam.set(capPropId("FRAME_WIDTH"), args.width)
-        cam.set(capPropId("FRAME_HEIGHT"), args.height)
-        cam.set(capPropId("FPS"), 15)
-    print("Initialization complete.")
-
-    while(True):
-        # Capture frame-by-frame
-        frames.clear()
-        for cam in cams:
-            ret, frame = cam.read()
-            # frame = f.unwarp(frame)
-            frames.append(frame)
-        # Our operations on the frame come here
-        # Display the resulting frame
-        if args.stitch:
-            # s = CVStitcher(args.debug)
-            s = Stitcher(args.debug)
-            pano = s.stitch(frames)
-            # cv2.resize(pano, (320, 180), interpolation=cv2.INTER_AREA)
-            cv2.imshow("pano", pano)
-        if args.debug:
-            for i in range(len(cams)):
-                cv2.imshow('frame' + str(i),
-                           cv2.resize(frames[i], (480, 270),
-                                      interpolation=cv2.INTER_AREA))
-            for cam in cams:
-                print(decode_fourcc(cam.get(cv2.CAP_PROP_FOURCC)))
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-    # When everything done, release the capture
-    for cam in cams:
-        cam.release()
-    cv2.destroyAllWindows()
+    c = cameraSystem(args.debug, args.stitch, args.width, args.height)
+    c.process()
 
 
 main()
